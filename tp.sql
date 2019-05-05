85568dfe3c39b43bb6060929e4b6026209424b04
Tomás Arjovsky <t.arjovsky@gmail.com>
2019-05-05 14:58:14 -0300

    Ejercicio 1 con calle y altura + notebook de limpieza

--- tp.sql
+++ tp.sql
@@ -4,8 +4,11 @@ CREATE EXTENSION postgis_topology;
 
 -- El CSV limpio del mapa de la ciudad tiene coordenadas X, Y (longitud, latitud).
 create table paradas_raw (
+	id_paradas bigint,
 	longitud float,
-	latitud float
+	latitud float,
+	calle varchar,
+	altura int
 );
 
 COPY paradas_raw
@@ -20,32 +23,29 @@ select * from paradas_raw limit 5;
 -- Google maps representa las coordenadas como (Latitud, Longitud), orden inverso al
 -- de la ciudad. Representaremos nuestras paradas con esa convención.
 create table paradas as
-select ST_MakePoint(latitud, longitud) as coords from paradas_raw
-
--- Distancia en metros de todos los puntos a la facultad de ingeniería.
-select ST_Distance(ST_MakePoint(-34.617454, -58.368293), coords)*111000 as distancia
-from paradas
-order by distancia asc;
+select *, ST_MakePoint(latitud, longitud) as coords from paradas_raw;
 
 -- Devuelve las paradas a menos de max_distancia respecto de la parada_objetivo.
 create or replace function paradas_cercanas(parada_objetivo geometry, max_distancia float)
-  returns table
-    (distance float)
+  returns table (
+    calle varchar,
+    altura int,
+    distancia float)
   as $$
   declare
     metros_por_grado constant float := 111000;
   begin
       return query
 	with c as (
-          select ST_Distance(parada_objetivo, coords)*metros_por_grado as distancia
+          select *, ST_Distance(parada_objetivo, coords)*metros_por_grado as distancia
 	  from paradas
         )
-        select *
+        select c.calle, c.altura, c.distancia
         from c
-        where distancia < max_distancia;
+        where c.distancia < max_distancia;
   end; $$
 language 'plpgsql';
 
 -- Las paradas a menos de 200 metros de FIUBA (PC).
 -- Las coordenadas de FIUBA fueron obtenidas de Google Maps.
-select paradas_cercanas(ST_MakePoint(-34.617454, -58.368293), 200);
+select * from paradas_cercanas(ST_MakePoint(-34.617454, -58.368293), 200);
