42dac1f6c7340ac88ec3424803cd8f6b3d2c74a5
jazmin.ferreiro <jazmin.ferreiro@despegar.com>
2019-05-12 21:23:08 -0300

    adding lines

--- tp.sql
+++ tp.sql
@@ -2,9 +2,9 @@
 CREATE EXTENSION postgis;
 CREATE EXTENSION postgis_topology;
 
--- El CSV limpio del mapa de la ciudad tiene coordenadas X, Y (longitud, latitud).
+--- El CSV limpio del mapa de la ciudad tiene coordenadas X, Y (longitud, latitud).
 create table paradas_raw (
-	id_paradas bigint,
+	id_parada bigint primary key,
 	longitud float,
 	latitud float,
 	calle varchar,
@@ -12,25 +12,49 @@ create table paradas_raw (
 );
 
 COPY paradas_raw
-FROM '/home/tomas/Desktop/BDD/TP/datasets/paradas-de-colectivo-clean.csv'
+FROM '/home/jazminferreiro/jaz/fiuba/baseDeDatos/tp/bdd-tp/datasets/paradas-de-colectivo-clean.csv'
+--FROM '/home/tomas/Desktop/BDD/TP/datasets/paradas-de-colectivo-clean.csv'
 DELIMITER ',' 
 CSV HEADER;
 
--- Observamos el contenido de las primeras filas.
+
+-- Observamos el contenido de las primeras filas de paradas.
 select * from paradas_raw limit 5;
+
+
+
+create table lineas_raw (
+	linea int not null,
+	id_parada int references paradas_raw(id_parada),
+	primary key(linea, id_parada));
+
+
+COPY lineas_raw
+FROM '/home/jazminferreiro/jaz/fiuba/baseDeDatos/tp/bdd-tp/datasets/lineas-por-paradas-clean.csv'
+DELIMITER ',' 
+CSV HEADER;
+
+-- Observamos el contenido de las primeras filas de lineas.
+select * from lineas_raw limit 5;
+
 
 -- Creamos una tabla con las los puntos correspondientes a cada parada.
 -- Google maps representa las coordenadas como (Latitud, Longitud), orden inverso al
 -- de la ciudad. Representaremos nuestras paradas con esa convención.
 create table paradas as
-select *, ST_MakePoint(latitud, longitud) as coords from paradas_raw;
+select p.id_parada, ST_MakePoint(latitud, longitud) as coords , calle, altura, linea 
+from paradas_raw p
+inner join lineas_raw l
+on p.id_parada = l.id_parada
 
+DROP FUNCTION IF EXISTS  paradas_cercanas
 -- Devuelve las paradas a menos de max_distancia respecto de la parada_objetivo.
 create or replace function paradas_cercanas(parada_objetivo geometry, max_distancia float)
   returns table (
     calle varchar,
     altura int,
-    distancia float)
+    distancia float,
+    linea int)
   as $$
   declare
     metros_por_grado constant float := 111000;
@@ -40,12 +64,14 @@ create or replace function paradas_cercanas(parada_objetivo geometry, max_distan
           select *, ST_Distance(parada_objetivo, coords)*metros_por_grado as distancia
 	  from paradas
         )
-        select c.calle, c.altura, c.distancia
+        select c.calle, c.altura, c.distancia, c.linea
         from c
         where c.distancia < max_distancia;
   end; $$
 language 'plpgsql';
 
+
+
 -- Las paradas a menos de 200 metros de FIUBA (PC).
 -- Las coordenadas de FIUBA fueron obtenidas de Google Maps.
-select * from paradas_cercanas(ST_MakePoint(-34.617454, -58.368293), 200);
+select calle, altura, linea from paradas_cercanas(ST_MakePoint(-34.617454, -58.368293), 200);
