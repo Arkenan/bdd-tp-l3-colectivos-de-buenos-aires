2f52d161f1c2d5d0aa7f8f7e5ced43992e8ebefa
Tomás Arjovsky <t.arjovsky@gmail.com>
2019-06-05 18:00:17 -0300

    Resuelto ejercicio 3.

--- tp3.sql
+++ tp3.sql
@@ -5,87 +5,43 @@
 -- parada y nombre del ramal que pasa por la parada, únicamente para aquellas paradas que no
 -- sean recorridas por todos los ramales de la línea.
 
--- Primero hago una tabla con la cuenta de, para cada parada, cuántos recorridos 
--- (línea + sentido)de un colectivo pasan por allí. 
+create or replace function paradas_incompletas(linea_objetivo int)
+  returns table (
+    id_parada bigint,
+    calle varchar,
+    ramal varchar)
+  as $$
+  begin
+    return query
+	with paradas_incompletas as (
+		select distinct(ppr.id_parada) from paradas_por_recorrido as ppr
+		where ppr.linea = linea_objetivo
+		group by ppr.id_parada, sentido
+		having count(ppr.ramal) < (
+			select count(distinct(ppr.ramal)) 
+			from paradas_por_recorrido as ppr
+			where ppr.linea = linea_objetivo
+		)
+	)
+	select distinct(ppr.id_parada), ppr.calle, ppr.ramal
+    from paradas_por_recorrido as ppr
+    where exists(
+   		select 1
+   		from paradas_incompletas as pi
+   		where pi.id_parada = ppr.id_parada
+  	) and ppr.linea = linea_objetivo;
+  end; $$
+language 'plpgsql';
 
+-- Ejemplo: paradas del 60 por las cuales no pasan todos los ramales.
+select * from paradas_incompletas(60);
 
--- pruebas -------------------------------------------------------------------------------------
+-- Ejemplo: paradas del 130 por las cuales no pasan todos los ramales.
+select * from paradas_incompletas(130) order by id_parada;
 
-select * 
-from paradas 
-where calle like '%URIBURU%' and linea = 93;
+select distinct(ramal) from
+paradas_por_recorrido where linea = 130
 
--- Vemos que la parada 1001045 es la única parda del 93 sobre uriburu, que solo va en un sentido.
-
-select *
-from recorridos
-where linea = 93;
-
--- Vemos que el 93 tiene tres ramales, 6 recorridos (ida y vuelta c/u).
-
-select * from paradas_por_recorrido
-where linea = 93 and id_parada = 1001045;
-
-select * 
-from paradas 
-where upper(calle) like '%LAS HERAS%' and linea = 93 
-and altura < 4000 and altura > 3400;
-
-select id_parada, count(distinct(ramal)) from paradas_por_recorrido
-where linea = 60
-group by id_parada
-having count(distinct(ramal)) < (select count(distinct(ramal)) from recorridos where linea = 60);
-
-
--- fin pruebas ----------------------------------------------------------------------------------
-
-
---- Pruebas del problema que parecería venir del ej2.
-
--- Paradas del 60 en Luis maria campos y 11 de septiembre
-select * 
-from paradas 
-where linea = 60 
-	and upper(calle) like '%CAMPOS%'
-	and altura > 1300
-	
--- La parada 1001996 está en LMC al 1395 (Esquina zabala). Solo los ramales que pasan por LMC deberían pasar por esa.
-
-select  * 
-from paradas_por_recorrido
-where linea = 60 and ramal = 'RAMAL C';
-
-select * from paradas where linea = 60;
-
--- El 60 tiene 15 ramales.
-
-select count(distinct(ramal))
-from paradas_por_recorrido
-where linea = 60;
-
--- 12 de esos ramales pasan por Luis María Campos.
-select id_parada, count(distinct(ramal)) 
-from paradas_por_recorrido
-where 
-	linea = 60 
-	and calle like '%CAMPOS%' 
-group by id_parada
-
-select id_parada, count(distinct(ramal)) 
-from paradas_por_recorrido
-where 
-	linea = 60 
-	and upper(calle) like '%CABILDO%' 
-group by id_parada
-
-
-select id_parada, count(distinct(ramal)) 
-from paradas_por_recorrido
-where 
-	linea = 60
-group by id_parada
-
-
-select * from paradas where linea = 60 and upper(calle) like '%CABILDO%'
--- Esto nos muestra que los 15 ramales del 60 pasan por las paradas de Luis María Campos. 
--- Eso significa indefectiblemente que tenemos un problema en el armado de paradas_por_recorrido.
+select * from 
+paradas_por_recorrido
+where id_parada = 1002111 and linea = 130
